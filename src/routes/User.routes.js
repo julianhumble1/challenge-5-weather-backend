c0d57d559ac2d1a1b16f737ecd325ddeb8bab0fe
Julian Humble <humblejulian@gmail.com>
2024-06-15 18:24:38 +0100

    added controller to User router and brief test with postman

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,18 +1,21 @@
 import { Router } from "express";
+import UserController from "../controllers/User.controller.js";
 
 export default class UserRoutes {
 
     #router;
     #routeStartPoint;
+    #controller
 
-    constructor(routeStartPoint = "/") {
+    constructor(controller = new UserController(), routeStartPoint = "/") {
+        this.#controller = controller;
         this.#routeStartPoint = routeStartPoint;
         this.#router = new Router();
         this.#initialiseRoutes();
     }
 
     #initialiseRoutes = () => {
-        this.#router.get("/", (req, res) => res.send("Getting todos"))
+        this.#router.get("/", this.#controller.getTodos)
     }  
 
     getRouter = () => {
