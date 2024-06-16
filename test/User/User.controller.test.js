6cdaaa070a1b6ff776411aa2b5fa59f9e7bdaf12
Julian Humble <humblejulian@gmail.com>
2024-06-16 15:30:46 +0100

    addNewUser controller test 3 passed

--- test/User/User.controller.test.js
+++ test/User/User.controller.test.js
@@ -6,62 +6,101 @@ import User from "../../src/models/User.model.js";
 import UserValidator from "../../src/middleware/UserValidator.js";
 
 describe("UserController tests", () => {
-    describe("successful request tests", () => {
-
+    describe("addNewUser request tests", () => {
         let userController;
         let userServices;
-        let req, res, next;
-        let userValidatorStub;
-        let newUser;
 
         beforeEach(() => {
             userServices = {
-                addNewUser: sinon.stub()
-            }
-
+                    addNewUser: sinon.stub()
+                }
             userController = new UserController(userServices)
-
-            req = {
-                body:
-                    {"email": "user@example.com",
-                    "password": "password1!"}
-            }     
-            res = {
-                json: sinon.spy(),
-                status: sinon.stub().returnsThis()
-            }
-            next = sinon.spy();
-            userValidatorStub = sinon.stub(UserValidator, "handleValidationErrors").callsFake((req, res, next) => next)
-
-            newUser = {
-                    "email": "user@example.com",
-                    "password": "password1!",
-                    "favouriteLocations": [],
-                    "_id": "666eecdb4463cfb7134ef2ac",
-                    "__v": 0
-            }
         })
 
         afterEach(() => {
-            sinon.restore();
+                sinon.restore();
         })
 
-        it("should respond with new user in body if request is successful", async () => {
-            // Arrange
-            userServices.addNewUser.resolves(newUser);
-            // Act
-            await userController.addNewUser(req, res)
-            // Assert
-            expect(res.json.calledWith(newUser)).to.be.true;
+        describe("successful request tests", () => {
+    
+            let req, res, next;
+            let userValidatorStub;
+            let newUser;
+
+            beforeEach(() => {
+    
+                req = {
+                    body:
+                        {"email": "user@example.com",
+                        "password": "password1!"}
+                }     
+                res = {
+                    json: sinon.spy(),
+                    status: sinon.stub().returnsThis()
+                }
+                next = sinon.spy();
+                userValidatorStub = sinon.stub(UserValidator, "handleValidationErrors").callsFake((req, res, next) => next)
+    
+                newUser = {
+                        "email": "user@example.com",
+                        "password": "password1!",
+                        "favouriteLocations": [],
+                        "_id": "666eecdb4463cfb7134ef2ac",
+                        "__v": 0
+                }
+            })
+    
+            
+    
+            it("should respond with new user in body if request is successful", async () => {
+                // Arrange
+                userServices.addNewUser.resolves(newUser);
+                // Act
+                await userController.addNewUser(req, res)
+                // Assert
+                expect(res.json.calledWith(newUser)).to.be.true;
+            })
+    
+            it("should respond with status code 201 if request is successful", async () => {
+                // Arrange
+                userServices.addNewUser.resolves(newUser);
+                // Act
+                await userController.addNewUser(req, res);
+                // Assert
+                expect(res.status.calledWith(201)).to.be.true;
+            })
         })
 
-        it("should respond with status code 201 if request is successful", async () => {
-            // Arrange
-            userServices.addNewUser.resolves(newUser);
-            // Act
-            await userController.addNewUser(req, res);
-            // Assert
-            expect(res.status.calledWith(201)).to.be.true;
+        describe("unsuccessful request tests", () => {
+
+            let req, res, next;
+            let userValidatorStub;
+    
+            beforeEach(() => {
+    
+                req = {
+                    body:
+                        {"email": "user@example.com",
+                        "password": "password1!"}
+                }     
+                res = {
+                    json: sinon.spy(),
+                    status: sinon.stub().returnsThis()
+                }
+                next = sinon.spy();
+                userValidatorStub = sinon.stub(UserValidator, "handleValidationErrors").callsFake((req, res, next) => next)
+
+            })
+
+            it("should respond with 500 code if addNewUser service throws error", async () => {
+                // Arrange
+                userServices.addNewUser.rejects(new Error("service error"))
+                // Act
+                await userController.addNewUser(req, res)
+                // Assert
+                expect(res.status.calledWith(500)).to.be.true;
+            })
         })
+
     })
 })
