702be37870ae9b10e9b4eec4ae5febcccb8ff4e0
Julian Humble <humblejulian@gmail.com>
2024-06-16 16:13:03 +0100

    addNewUser service test 2 passed

--- test/User/User.service.test.js
+++ test/User/User.service.test.js
@@ -13,12 +13,13 @@ describe("User service tests", () => {
 
         let userStub;
         let newUser;
-        let saveStub;
+        let saveStub
+        
 
         beforeEach(() => {
-            userStub = sinon.stub(User.prototype, "constructor").callsFake((newUser) => {{}})
-
-            saveStub = sinon.stub(User.prototype, "save")
+            userStub = sinon.stub(User.prototype, "constructor").callsFake((newUser) => { { } })
+            
+            saveStub = sinon.stub(User.prototype, "save");
             
             newUser = {"email": "user@example.com",
                         "password": "password1!"}
@@ -26,7 +27,7 @@ describe("User service tests", () => {
         })
 
         afterEach(() => {
-            userStub.restore();
+            sinon.restore();
         })
 
         it("should call save and return the result when a valid User is added ", async () => {
@@ -44,6 +45,22 @@ describe("User service tests", () => {
             const result = await userService.addNewUser(newUser)
             // Assert
             expect(result).to.equal(newUserDoc);
+        })
+
+        it("should throw an error when save fails", async () => {
+            // Arrange
+            const invalidUser = { email: "" };
+            const error = new Error("Invalid user")
+            saveStub.throws(error);
+
+            // Act // Assert
+            try {
+                await userService.addNewUser(invalidUser);
+                assert.fail("Expected error was not thrown")
+            } catch (e) {
+                expect(e).to.equal(error);
+            }
+            
         })
     })
 })
