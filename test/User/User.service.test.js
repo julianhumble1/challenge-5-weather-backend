cbaaee2ce2937f47454a37e0a6e9f9207a2dd44e
Julian Humble <humblejulian@gmail.com>
2024-06-18 00:02:00 +0100

    service loginUser test 3 passed

--- test/User/User.service.test.js
+++ test/User/User.service.test.js
@@ -72,6 +72,7 @@ describe("User service tests", () => {
 
         beforeEach(() => {
             findUserStub = sinon.stub(User, "findOne")
+            bcryptStub = sinon.stub(bcrypt, "compareSync")
             jwtStub = sinon.stub(jwt, "sign").returns("accessToken")
         })
 
@@ -111,7 +112,20 @@ describe("User service tests", () => {
             } catch (e) {
                 expect(e.message).to.equal(error.message);
             }
-            
+        })
+
+        it("should return a null access token if username doesn't match email", async () => {
+            // Arrange
+            const invalidUser = {
+                email: "email3@email.com",
+                password: "password1!"
+            };
+            findUserStub.returns({})
+            bcryptStub.returns(false);
+            // Act
+            const response = await userService.loginUser(invalidUser)
+            // Assert
+            expect(response.accessToken).to.equal(null)
         })
     })
 })
