db21868e6a5dfcf525b262286f3be3c5727b983c
Julian Humble <humblejulian@gmail.com>
2024-06-19 10:17:55 +0100

    service addFavLocation test 4 passed

--- test/User/User.service.test.js
+++ test/User/User.service.test.js
@@ -500,5 +500,25 @@ describe("User service tests", () => {
                 expect(e.message).to.equal(error.message);
             }
         })
+
+        it("should throw location not already in favourites error if location is not already in favourites", async () => {
+            // Arrange
+            const requestBody = {
+                email: "email2@email.com",
+                locationId: "2345678"
+             };
+            findUserStub.resolves({
+                id: "666ebf51cdf1cff8e67b6fc4",
+                favouriteLocations: ["1234567"]
+            })
+            const error = new Error("Location already not in favourites")
+            // Act // Assert
+             try {
+                await userService.removeFavLocation(requestBody)
+                expect.fail("Expected error was not thrown")
+            } catch (e) {
+                expect(e.message).to.equal(error.message);
+            }
+        })
     })
 })
